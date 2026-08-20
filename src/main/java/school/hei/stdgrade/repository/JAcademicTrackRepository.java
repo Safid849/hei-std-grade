51151e80a41ba64dea7d88b30a414f3df2e2bd47
Zinedis <nantenainabakari@gmail.com>
2026-08-20 22:00:09 +0300

    feat(academic-track): add findByCode to repository

--- src/main/java/school/hei/stdgrade/repository/JAcademicTrackRepository.java
+++ src/main/java/school/hei/stdgrade/repository/JAcademicTrackRepository.java
@@ -1,6 +1,11 @@
 package school.hei.stdgrade.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.hei.stdgrade.repository.model.JAcademicTrack;
 
-public interface JAcademicTrackRepository extends JpaRepository<JAcademicTrack, String> {}
+public interface JAcademicTrackRepository extends JpaRepository<JAcademicTrack, String> {
+  Optional<JAcademicTrack> findByCode(String code);
+
+  boolean existsByCodeAndIdNot(String code, String id);
+}
