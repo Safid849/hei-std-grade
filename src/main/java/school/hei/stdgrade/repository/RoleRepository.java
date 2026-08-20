62ba54bd8d9c82e48e5eb958b96ab4892ed3e57c
DavFilsDev <miharisoadavidfils@gmail.com>
2026-08-20 10:19:39 +0300

    fix(repository): remove duplicated RoleRepository, use JRoleRepository

--- src/main/java/school/hei/stdgrade/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package school.hei.stdgrade.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import school.hei.stdgrade.repository.model.JRole;
-
-@Repository
-public interface RoleRepository extends JpaRepository<JRole, String> {}
--- src/main/java/school/hei/stdgrade/service/RoleService.java
+++ src/main/java/school/hei/stdgrade/service/RoleService.java
@@ -4,13 +4,13 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.stdgrade.model.Role;
-import school.hei.stdgrade.repository.RoleRepository;
+import school.hei.stdgrade.repository.JRoleRepository;
 import school.hei.stdgrade.repository.mapper.JRoleMapper;
 
 @Service
 @AllArgsConstructor
 public class RoleService {
-  private final RoleRepository repository;
+  private final JRoleRepository repository;
   private final JRoleMapper mapper;
 
   public List<Role> findAll() {
