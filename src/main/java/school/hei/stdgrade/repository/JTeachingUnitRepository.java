dc0781e432d7ce42ce463544853edaae7ebc0f03
Zinedis <nantenainabakari@gmail.com>
2026-08-21 21:45:45 +0300

    feat(teaching-unit): add findByCode and existsByCodeAndIdNot in repository

--- src/main/java/school/hei/stdgrade/repository/JTeachingUnitRepository.java
+++ src/main/java/school/hei/stdgrade/repository/JTeachingUnitRepository.java
@@ -1,6 +1,11 @@
 package school.hei.stdgrade.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.hei.stdgrade.repository.model.JTeachingUnit;
 
-public interface JTeachingUnitRepository extends JpaRepository<JTeachingUnit, String> {}
+public interface JTeachingUnitRepository extends JpaRepository<JTeachingUnit, String> {
+  Optional<JTeachingUnit> findByCode(String code);
+
+  boolean existsByCodeAndIdNot(String code, String id);
+}
